e010e07ee7a47af5b9599a9ec74ba30423318a3a
luisangel072 <luis.delatorre5745@alumnos.udg.mx>
2025-11-01 09:28:19 -0600

    acortar linea

--- pipePy/main.py
+++ pipePy/main.py
@@ -20,7 +20,7 @@ usar_linter = input("¿Quieres análisis de código estático? (S/n): ") or "s"
 steps_linter = ''
 linter_pkg = ''
 if usar_linter.lower() == 's':
-    linter = input("¿Qué herramienta usarás para código estático? ('flake8', 'pylint', etc. por defecto: 'flake8'): ") or "flake8"
+    linter = input("Elige la herramienta para análisis estático ('flake8', 'pylint', etc. por defecto: flake8): ") or "flake8"
     linter_pkg = linter.split()[0]
     cmd_linter = input(f"Comando para análisis estático (por defecto: '{linter} .'): ") or f"{linter} ."
     steps_linter = f"""
